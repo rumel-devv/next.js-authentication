e2da5e5b8d8477c7ed022b79399a26e1839ced08
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-22 17:55:30 +0600

    changed navbar style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,39 +18,37 @@ const Navbar = () => {
   const pathname = usePathname();
 
   const linkStyle = (path) =>
-    `flex items-center gap-1 md:gap-2 px-2 md:px-3 py-2 rounded-lg transition-all duration-200 text-sm md:text-base ${
+    `flex items-center gap-2 px-3 py-2 rounded-lg transition-all duration-200 text-sm md:text-base whitespace-nowrap ${
       pathname === path
         ? "bg-white text-green-700"
         : "text-white hover:bg-white/20"
     }`;
 
   return (
-    <div className="navbar bg-gray-900 shadow-md flex justify-between px-20">
+          <div className="bg-gray-900 shadow-md py-2">
+  <div className="w-10/12  mx-auto px-4 flex items-center justify-between">
+    
+    {/* Logo */}
+    <Link href="/">
+      <h2 className="text-white font-bold text-xl md:text-2xl">
+        Keen<span className="text-green-500">Keeper</span>
+      </h2>
+    </Link>
 
-      {/* Logo */}
-      <div className="navbar-start">
-        <Link href="/">
-          <h2 className="text-white font-bold text-xl md:text-2xl">
-            Keen<span className="text-green-500">Keeper</span>
-          </h2>
-        </Link>
-      </div>
+    {/* Menu */}
+    <ul className="flex items-center gap-2 md:gap-4 font-semibold">
+      {navLinks.map((link) => (
+        <li key={link.path} className="flex ">
+          <Link href={link.path} className={linkStyle(link.path)}>
+            {link.icon}
+            <span className="hidden md:inline">{link.name}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
 
-      {/* Menu */}
-      <div className="navbar-end">
-        <ul className="menu menu-horizontal   gap-1 md:gap-3 lg:gap-4 font-semibold">
-          {navLinks.map((link) => (
-            <li  key={link.path}>
-              <Link href={link.path} className={linkStyle(link.path)}>
-                {link.icon}
-                <span className="hidden md:inline">{link.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-    </div>
+  </div>
+</div>
   );
 };
 
